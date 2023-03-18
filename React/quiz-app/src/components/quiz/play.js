97f23e9c72f63d8a2abf8f927037fc9d5ac86576
Siddhesh Shinde <shindesiddhesh13062003@gmail.com>
2023-03-18 19:52:32 +0530

    Quiz ui done

--- React/quiz-app/src/components/quiz/play.js
+++ React/quiz-app/src/components/quiz/play.js
@@ -1,44 +1,117 @@
 import React, {Fragment} from "react";
 import {Helmet} from 'react-helmet';
 import Icon from '@mdi/react';
+
 import {mdiTimerOutline, mdiChevronDoubleLeft, mdiChevronDoubleRight, mdiExitToApp, mdiSetCenter } from '@mdi/js';
+import {MCQ} from '../../question';
 
 class Play extends React.Component{
   
-    // constructor(props) {
-    //     super(props);
-        
-    //   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      numbers: [],
+      questions: MCQ,
+      currentQuestion: {},
+      nextQuestion: {},
+      previousQuestion: {},
+      answer: '',
+      currentQuestionIndex: 0,
+      correctAnswer: 0,
+      wrongAnswer: 0,  
+    };
+  }
+  
+
+  
+
+  generateNumbers = () => {
+    if (this.state.numbers.length > 0) {
+      return;
+    }
+    let numArray = [];
+    while (numArray.length < 30) {
+      let randomNumber = Math.floor(Math.random() * 30) + 1; // generate a random number between 1 and 100
+      if (!numArray.includes(randomNumber)) {
+        numArray.push(randomNumber);
+      }
+    }
+    this.setState({ numbers: numArray }, () => {
+      setTimeout(() => {
+        if (this.state.numbers == null) {
+          console.log("Error: state not updated");
+        }
+      }, 1000);
+    });
+
+    console.log("hello world")
+  };
+
+
+  componentDidMount() {
+    this.generateNumbers();
+    const {questions, currentQuestion, nextQuestion, previousQuestion} = this.state;
+    this.displayQuestions(questions, currentQuestion, nextQuestion, previousQuestion)
+  }
+
+  displayQuestions = (ques = this.state.questions, currentQuestion, nextQuestion, previousQuestion) =>{
+   
+
+     
+    currentQuestion = ques[this.state.currentQuestionIndex]
+    nextQuestion = ques[this.state.currentQuestionIndex + 1]
+    previousQuestion = ques[this.state.currentQuestionIndex - 1]
+    console.log("the numbers are", this.state.numbers)
+   
+    this.setState({
+     
+      currentQuestion,
+      nextQuestion,
+      previousQuestion
     
+    }, () => {
+     
+      console.log("The state currentquestion are:", this.state.currentQuestion)
+    });
+
+    
+}
+      
+  
       render() {
+        let {currentQuestion} = this.state
+        console.log("The current question",currentQuestion?.options)
+        let arr = currentQuestion?.options
+        console.log("The array is:", arr)
         return (
+         
           <Fragment>
+             
             <Helmet><title>Quiz</title></Helmet>
             <div className="questions">
             <div className="lifeline-container">
                 <p>
-                    <span className="lifeline-icon"><Icon path={mdiSetCenter} size={1} /></span>2
+                    <span className="lifeline-icon"><Icon path={mdiSetCenter} size={1} /></span><span className="lifeline">2</span>
                 </p>
             </div>
             <div>
                 <span>1/30</span>
                 <span> 2:30 <Icon path={mdiTimerOutline} size={1} /></span>
             </div>
-            <h5>Google was founded in what year</h5>
+            <h5>{currentQuestion["question"]}</h5>
             <div className="options-container">
-                <p className="options">1997</p>
+                <p className="options">{arr[0]}</p>
                 <p className="options">1998</p>
             </div>
             <div className="options-container">
-                <p className="options">1997</p>
-                <p className="options">1998</p>
+                <p className="options">1999</p>
+                <p className="options">2000</p>
             </div>
 
             <div className="button-container">
-                <button><Icon path={mdiChevronDoubleLeft} size={0.8} /> Previous</button>
+                <button><Icon className="button-icon" path={mdiChevronDoubleLeft} size={0.8} /> Previous</button>
                 <button>Next <Icon path={mdiChevronDoubleRight} size={0.8} /></button>
-                <button>
-<Icon path={mdiExitToApp} size={0.8} /> Quit</button>
+                <button><Icon path={mdiExitToApp} size={0.8} /> Quit</button>
             </div>
             </div>
 
---
+++ React/quiz-app/src/question.js
@@ -0,0 +1,406 @@
+export const MCQ = [
+  {
+    question: "What is the capital of India?",
+    options: ["New Delhi", "Mumbai", "Kolkata", "Chennai"],
+    answer: "New Delhi",
+  },
+  {
+    question: "What is the currency of India?",
+    options: ["Rupee", "Dollar", "Euro", "Yen"],
+    answer: "Rupee",
+  },
+  {
+    question: "What is the national animal of India?",
+    options: ["Tiger", "Lion", "Elephant", "Leopard"],
+    answer: "Tiger",
+  },
+  {
+    question: "What is the national flower of India?",
+    options: ["Lotus", "Rose", "Jasmine", "Marigold"],
+    answer: "Lotus",
+  },
+  {
+    question: "What is the official language of India?",
+    options: ["English", "Hindi", "Bengali", "Telugu"],
+    answer: "Hindi",
+  },
+  {
+    question: "What is the name of the world's largest democracy?",
+    options: ["China", "India", "United States", "Japan"],
+    answer: "India",
+  },
+  {
+    question: "Which river is considered sacred in Hinduism?",
+    options: ["Ganges", "Nile", "Yangtze", "Amazon"],
+    answer: "Ganges",
+  },
+  {
+    question:
+      "What is the name of the world's largest film industry by number of films produced?",
+    options: ["Hollywood", "Bollywood", "Tollywood", "Lollywood"],
+    answer: "Bollywood",
+  },
+  {
+    question:
+      "What is the name of the world's tallest statue located in India?",
+    options: [
+      "Statue of Unity",
+      "Christ the Redeemer",
+      "Great Sphinx of Giza",
+      "Mount Rushmore",
+    ],
+    answer: "Statue of Unity",
+  },
+  {
+    question:
+      "Which Indian freedom fighter is also known as 'The Father of the Nation'?",
+    options: [
+      "Mahatma Gandhi",
+      "Jawaharlal Nehru",
+      "Sardar Vallabhbhai Patel",
+      "Subhas Chandra Bose",
+    ],
+    answer: "Mahatma Gandhi",
+  },
+  {
+    question: "What is the name of the highest mountain peak in India?",
+    options: [
+      "Mount Everest",
+      "Kangchenjunga",
+      "Nanda Devi",
+      "Mount Godwin-Austen",
+    ],
+    answer: "Kangchenjunga",
+  },
+  {
+    question: "What is the name of the largest desert in India?",
+    options: [
+      "Thar Desert",
+      "Great Basin Desert",
+      "Mojave Desert",
+      "Atacama Desert",
+    ],
+    answer: "Thar Desert",
+  },
+  {
+    question: "Which Indian state is known as 'The Land of Rising Sun'?",
+    options: ["Assam", "Arunachal Pradesh", "Manipur", "Meghalaya"],
+    answer: "Arunachal Pradesh",
+  },
+  {
+    question: "What is the name of the world's largest delta located in India?",
+    options: ["Ganges Delta", "Amazon Delta", "Nile Delta", "Yangtze Delta"],
+    answer: "Ganges Delta",
+  },
+  {
+    question: "What is the name of the longest river in India?",
+    options: ["Ganges", "Indus", "Brahmaputra", "Godavari"],
+    answer: "Ganges",
+  },
+  {
+    question: "Which Indian state is known as 'The Land of Spices'?",
+    options: ["Kerala", "Tamil Nadu", "Andhra Pradesh", "Karnataka"],
+    answer: "Kerala",
+  },
+  {
+    question:
+      "What is the name of the world's largest film studio located in India?",
+    options: [
+      "Ramoji Film City",
+      "Hengdian World Studios",
+      "Pinewood Studios",
+      "Universal Studios",
+    ],
+    answer: "Ramoji Film City",
+  },
+  {
+    question: "Which Indian city is known as 'The Pink City'?",
+    options: ["Jaipur", "Udaipur", "Jodhpur", "Bikaner"],
+    answer: "Jaipur",
+  },
+  {
+    question: "What is the name of the largest mosque in India?",
+    options: [
+      "Jama Masjid",
+      "Taj-ul-Masajid",
+      "Badshahi Mosque",
+      "Fatehpuri Masjid",
+    ],
+    answer: "Jama Masjid",
+  },
+  {
+    question: "What is the name of the largest wildlife sanctuary in India?",
+    options: [
+      "Jim Corbett National Park",
+      "Kanha National Park",
+      "Ranthambore National Park",
+      "Sunderbans National Park",
+    ],
+    answer: "Jim Corbett National Park",
+  },
+  {
+    question: "What is the smallest planet in our solar system?",
+    options: ["Earth", "Mars", "Venus", "Mercury"],
+    answer: "Mercury",
+  },
+  {
+    question: "What is the name of the currency used in Japan?",
+    options: ["Yen", "Won", "Dollar", "Euro"],
+    answer: "Yen",
+  },
+  {
+    question: "What is the capital city of France?",
+    options: ["Paris", "London", "Berlin", "Rome"],
+    answer: "Paris",
+  },
+  {
+    question: "What is the name of the famous tower in Pisa, Italy?",
+    options: [
+      "Eiffel Tower",
+      "Big Ben",
+      "Leaning Tower of Pisa",
+      "Tower Bridge",
+    ],
+    answer: "Leaning Tower of Pisa",
+  },
+  {
+    question:
+      "What is the name of the famous bridge connecting San Francisco and Marin County?",
+    options: [
+      "Brooklyn Bridge",
+      "Golden Gate Bridge",
+      "London Bridge",
+      "Sydney Harbour Bridge",
+    ],
+    answer: "Golden Gate Bridge",
+  },
+  {
+    question: "What is the name of the largest ocean in the world?",
+    options: [
+      "Pacific Ocean",
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Arctic Ocean",
+    ],
+    answer: "Pacific Ocean",
+  },
+  {
+    question: "What is the capital city of Australia?",
+    options: ["Sydney", "Melbourne", "Canberra", "Perth"],
+    answer: "Canberra",
+  },
+  {
+    question:
+      "What is the name of the famous statue located in Rio de Janeiro, Brazil?",
+    options: [
+      "The Christ the Redeemer",
+      "The Statue of Liberty",
+      "The Colosseum",
+      "The Sphinx",
+    ],
+    answer: "The Christ the Redeemer",
+  },
+  {
+    question:
+      "What is the name of the famous clock tower located in London, England?",
+    options: ["Big Ben", "Elgin Marbles", "The Parthenon", "The Colosseum"],
+    answer: "Big Ben",
+  },
+  {
+    question: "What is the capital city of Germany?",
+    options: ["Berlin", "Paris", "London", "Rome"],
+    answer: "Berlin",
+  },
+  {
+    question:
+      "What is the only planet in our solar system that rotates clockwise?",
+    options: ["Mars", "Venus", "Jupiter", "Uranus"],
+    answer: "Venus",
+  },
+  {
+    question:
+      "What is the mathematical term for the number that is equal to the sum of its proper divisors (excluding itself)?",
+    options: [
+      "Perfect Number",
+      "Amicable Number",
+      "Deficient Number",
+      "Abundant Number",
+    ],
+    answer: "Perfect Number",
+  },
+  {
+    question:
+      "What is the term used to describe a sequence of numbers in which each number is the sum of the two preceding numbers?",
+    options: [
+      "Geometric Sequence",
+      "Arithmetic Sequence",
+      "Fibonacci Sequence",
+      "Harmonic Sequence",
+    ],
+    answer: "Fibonacci Sequence",
+  },
+  {
+    question: "What is the number of digits in the number 'googol'?",
+    options: ["100", "1000", "100000", "100000000"],
+    answer: "100",
+  },
+  {
+    question: "What is the smallest prime number?",
+    options: ["2", "3", "5", "7"],
+    answer: "2",
+  },
+  {
+    question:
+      "What is the name of the theorem that states that the square root of two is an irrational number?",
+    options: [
+      "Fermat's Last Theorem",
+      "Pythagorean Theorem",
+      "Euclid's Elements",
+      "Pell's Equation",
+    ],
+    answer: "Pythagorean Theorem",
+  },
+  {
+    question:
+      "What is the name of the problem that asks to find the most efficient way to connect seven bridges in a city?",
+    options: [
+      "Traveling Salesman Problem",
+      "Bridge and Torch Problem",
+      "Knapsack Problem",
+      "Graph Coloring Problem",
+    ],
+    answer: "Bridge and Torch Problem",
+  },
+  {
+    question:
+      "What is the name of the equation that states that the sum of the squares of the first n natural numbers is equal to n(n+1)(2n+1)/6?",
+    options: [
+      "Euler's Formula",
+      "Fibonacci Formula",
+      "Sum of Squares Formula",
+      "Gauss' Formula",
+    ],
+    answer: "Sum of Squares Formula",
+  },
+  {
+    question:
+      "What is the number of faces, edges, and vertices in a tetrahedron?",
+    options: [
+      "4 faces, 6 edges, 4 vertices",
+      "4 faces, 8 edges, 6 vertices",
+      "6 faces, 12 edges, 8 vertices",
+      "4 faces, 4 edges, 4 vertices",
+    ],
+    answer: "4 faces, 4 edges, 4 vertices",
+  },
+  {
+    question: "What is the value of x if 2x + 3 = 7?",
+    options: ["2", "1", "0", "3"],
+    answer: "2",
+  },
+  {
+    question: "What is the square root of 256?",
+    options: ["16", "12", "18", "14"],
+    answer: "16",
+  },
+  {
+    question: "What is the value of x if x^2 + 4x + 4 = 0?",
+    options: ["-2, 2", "0, 4", "-4, 0", "2, -2"],
+    answer: "-2, 2",
+  },
+  {
+    question: "What is the value of x if log base 2 of x = 3?",
+    options: ["8", "16", "24", "32"],
+    answer: "8",
+  },
+  {
+    question: "What is the sum of the interior angles of a hexagon?",
+    options: ["720°", "540°", "360°", "180°"],
+    answer: "720°",
+  },
+  {
+    question: "What is the value of x if x / 4 = 12?",
+    options: ["48", "36", "24", "12"],
+    answer: "48",
+  },
+  {
+    question: "What is the value of x if 2x - 3 = 15?",
+    options: ["9", "12", "6", "8"],
+    answer: "9",
+  },
+  {
+    question: "What is the value of x if (x + 3)^2 = 16?",
+    options: ["-5, 5", "5, -5", "-7, 7", "7, -7"],
+    answer: "-5, 5",
+  },
+  {
+    question: "What is the value of x if x^3 + 8 = 0?",
+    options: ["2", "-2", "1", "-1"],
+    answer: "-2",
+  },
+  {
+    question: "What is the value of x if (x - 4)^2 = 9?",
+    options: ["1, 7", "-7, -1", "7, 1", "-1, -7"],
+    answer: "1, 7",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: C, F, J, O, ?",
+    options: ["N", "S", "U", "T"],
+    answer: "U",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: A, E, J, P, ?",
+    options: ["R", "V", "Y", "X"],
+    answer: "Y",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: C, F, J, O, T, ?",
+    options: ["X", "Y", "Z", "W"],
+    answer: "Z",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: B, G, N, U, ?",
+    options: ["Y", "Z", "D", "A"],
+    answer: "D",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: P, S, W, Z, ?",
+    options: ["B", "C", "D", "E"],
+    answer: "C",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: J, O, U, Z, ?",
+    options: ["F", "G", "H", "I"],
+    answer: "G",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: D, J, Q, Y, ?",
+    options: ["H", "I", "G", "F"],
+    answer: "H",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: B, F, K, P, ?",
+    options: ["U", "V", "W", "X"],
+    answer: "U",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: G, L, R, Y, ?",
+    options: ["F", "E", "D", "C"],
+    answer: "F",
+  },
+  {
+    question:
+      "What is the next term in the following alphanumeric series: A, F, M, T, ?",
+    options: ["Z", "Y", "X", "W"],
+    answer: "Z",
+  },
+];
